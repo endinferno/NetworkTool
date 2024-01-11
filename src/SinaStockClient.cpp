65e5d3493db1722d215e4deedc355ca9f30cbe7a
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-01-12 01:47:47 +0800

    fix: change the value of "Host" to be a const value

--- src/SinaStockClient.cpp
+++ src/SinaStockClient.cpp
@@ -23,7 +23,7 @@ HttpRequest SinaStockClient::ConstructStockRequest(const std::string& stockId)
     HttpRequest httpReq;
     httpReq.SetReqType(HttpRequest::ReqType::GET);
     httpReq.SetUrl(fmt::format("/list={}", stockId));
-    httpReq.AddHeader("Host", "hq.sinajs.cn");
+    httpReq.AddHeader("Host", sinaStockDomainName);
     httpReq.AddHeader("Referer", "http://finance.sina.com.cn");
     return httpReq;
 }
