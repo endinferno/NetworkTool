aa441f1e530ca135231bac1f4e072d6ea350b91b
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-01-30 12:22:43 +0800

    chore: add run-clang-tidy.sh script

---
+++ script/run-clang-tidy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rootDir=$(git rev-parse --show-toplevel)
+compileCommandDir=$rootDir/build/debug
+
+run-clang-tidy -p $compileCommandDir \
+    $rootDir/Main.cpp \
+    $rootDir/include/*.hpp \
+    $rootDir/include/Poller/*.hpp \
+    $rootDir/src/*.cpp \
+    $rootDir/src/Poller/*.cpp
