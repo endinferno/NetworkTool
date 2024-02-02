27ab99876883d8d141b34e3efbf58b04aefdff72
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-02 11:44:35 +0800

    fix: remove IPAddress.cpp and move implementation to IPAddress.hpp

--- include/IPAddress.hpp
+++ include/IPAddress.hpp
@@ -1,17 +1,37 @@
 #pragma once
 
-#include <cstdint>
+#include <arpa/inet.h>
 
+#include <array>
 #include <string>
 
 class IPAddress
 {
 public:
-    explicit IPAddress(uint32_t ipAddr);
-    explicit IPAddress(const std::string& ipAddr);
-    [[nodiscard]] uint32_t GetHostIpAddr() const;
-    [[nodiscard]] uint32_t GetNetIpAddr() const;
-    [[nodiscard]] std::string Stringify() const;
+    explicit IPAddress(uint32_t hostIpAddr)
+        : hostIp_(hostIpAddr)
+    {}
+    explicit IPAddress(const std::string& hostIpAddr)
+    {
+        uint32_t ipAddrInt = 0;
+        ::inet_pton(AF_INET, hostIpAddr.data(), &ipAddrInt);
+        hostIp_ = ntohl(ipAddrInt);
+    }
+    [[nodiscard]] uint32_t GetHostIpAddr() const
+    {
+        return hostIp_;
+    }
+    [[nodiscard]] uint32_t GetNetIpAddr() const
+    {
+        return htonl(hostIp_);
+    }
+    [[nodiscard]] std::string Stringify() const
+    {
+        uint32_t netIpAddr = htonl(hostIp_);
+        std::array<char, 20> ipAddr;
+        ::inet_ntop(AF_INET, &netIpAddr, ipAddr.data(), 20);
+        return { ipAddr.data() };
+    }
 
 private:
     uint32_t hostIp_;
