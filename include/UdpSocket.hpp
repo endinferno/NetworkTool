30ba8d87dde68ecb8e72bfb88dbad275acac53b7
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-02 14:29:00 +0800

    feat: support UdpSocket

---
+++ include/UdpSocket.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "Socket.hpp"
+
+class UdpSocket : public Socket
+{
+public:
+    UdpSocket()
+        : Socket(::socket(AF_INET, SOCK_DGRAM, 0))
+    {}
+    ~UdpSocket() override
+    {
+        ::close(GetFd());
+    }
+};
+
+using UdpSocketPtr = std::shared_ptr<UdpSocket>;
