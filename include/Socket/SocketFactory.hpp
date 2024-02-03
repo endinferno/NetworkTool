0fe113923f2a6a38cd9b83913ba8ffb0fd39b566
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-03 21:14:10 +0800

    reafctor: remove SocketFactory cpp file and move source code into hpp file

--- include/Socket/SocketFactory.hpp
+++ include/Socket/SocketFactory.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
-#include "Socket/Socket.hpp"
+#include <stdexcept>
+
+#include "Socket/TcpSocket.hpp"
+#include "Socket/UdpSocket.hpp"
 
 class SocketFactory
 {
 public:
     SocketFactory() = default;
-    SocketPtr Create(enum Socket::SocketType type);
+    SocketPtr Create(enum Socket::SocketType type)
+    {
+        switch (type) {
+        case Socket::SocketType::TCP: return std::make_shared<TcpSocket>();
+        case Socket::SocketType::UDP: return std::make_shared<UdpSocket>();
+        default: throw std::runtime_error("No such socket type\n");
+        }
+    }
     ~SocketFactory() = default;
 };
