22e7f1ce0f42527829f75c31041779f67913a468
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-03 21:12:15 +0800

    refactor: remove ConnectorFactory cpp file and move source code into hpp file

--- include/Connector/ConnectorFactory.hpp
+++ include/Connector/ConnectorFactory.hpp
@@ -1,12 +1,29 @@
 #pragma once
 
+#include <memory>
+#include <stdexcept>
+
 #include "Connector/Connector.hpp"
+#include "Connector/SslConnector.hpp"
+#include "Connector/TcpConnector.hpp"
+#include "Connector/UdpConnector.hpp"
 
 class ConnectorFactory
 {
 public:
     ConnectorFactory() = default;
     ConnectorPtr Create(EventPollerPtr& poller,
-                        enum Connector::ConnectorType type);
+                        enum Connector::ConnectorType type)
+    {
+        switch (type) {
+        case Connector::ConnectorType::TCP:
+            return std::make_unique<TcpConnector>(poller);
+        case Connector::ConnectorType::UDP:
+            return std::make_unique<UdpConnector>(poller);
+        case Connector::ConnectorType::SSL:
+            return std::make_unique<SslConnector>(poller);
+        default: throw std::runtime_error("No such socket type\n");
+        }
+    }
     ~ConnectorFactory() = default;
 };
