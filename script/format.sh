1a4b3d6c4785714cd4ab5e6130ad96d0117ffc48
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-22 16:24:29 +0800

    refactor: update format.sh

--- script/format.sh
+++ script/format.sh
@@ -2,7 +2,7 @@
 
 rootDir=$(git rev-parse --show-toplevel)
 
-clang-format -style=file -i $rootDir/Main.cpp
 find $rootDir/src -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
 find $rootDir/include -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
+find $rootDir/examples -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
 find $rootDir/test -regex '.*\.\(cpp\|hpp\)' -exec clang-format -style=file -i {} \;
