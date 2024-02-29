8947e4d3a34d3f02a7cef6452860462f7e52511b
Shaofeng Cen <censhaofeng@sjtu.edu.cn>
2024-02-29 12:40:33 +0800

    fix: close udp socket after using

--- src/HttpClient.cpp
+++ src/HttpClient.cpp
@@ -7,7 +7,7 @@ HttpClient::HttpClient(EventPollerPtr& poller)
 
 void HttpClient::Connect(const IPAddress& serverIp, const uint16_t& serverPort)
 {
-    // TODO: close DNS resolver UDP socket
+    resolver_.reset();
     tcpClient_.SetMessageCallback(
         [this]([[maybe_unused]] ChannelPtr& chan, const std::string& httpMsg) {
             OnMessage(httpMsg);
